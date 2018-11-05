256dff9a1cfc24b2c54974fa81e8683c532aa86a
Frank Fang <FrankFang1990@gmail.com>
2018-11-05 22:15:38 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+
 
 class App extends Component {
-  add1(){
-    this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  add3(){
-    this.props.onAdd3()
-  }
-  add4(){
-    this.props.onAdd4()
-  }
   render() {
     return (
       <div>
-        你点击了 <span id="value">{this.props.value}</span> 次
+        你点击了 <span id="value">{this.props.n}</span> 次
         <div>
-          <button id="add1" onClick={()=> this.add1()}>+1</button>
-          <button id="add2" onClick={()=> this.add2()}>+2</button>
-          <button id="add1IfOdd" onClick={()=>this.add3()}>如果是单数就+1</button>
-          <button id="add1After2Sec" onClick={()=>this.add4()}>两秒钟后+1</button>
+          <button id="add1" onClick={()=> this.props.add1()}>+1</button>
+          <button id="add2">+2</button>
+          <button id="add1IfOdd">如果是单数就+1</button>
+          <button id="add1After2Sec">两秒钟后+1</button>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+function getPartialStore(state){
+  return {
+    n: state.n
+  }
+}
+const actionCreator = {
+  add1: ()=>{
+    return {type:'add', payload: 1}
+  }
+}
+
+export default connect(getPartialStore,actionCreator)(App);
--- src/index.js
+++ src/index.js
@@ -4,46 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
+import { Provider } from "react-redux";
 
 const stateChanger = (state, action)=>{
   if(state === undefined){
-    return 0
+    return {n: 0}
   }else{
     if(action.type === 'add'){
-      var newState = state + action.payload
+      var newState = {n: state.n + action.payload}
       return newState
     }else{
       return state
     }
   }
-
-  return newState
 }
 const store = createStore(stateChanger)
-render()
-store.subscribe(()=>{
-  render()
-})
 
-function add3(){
-  if(store.getState() % 2 === 1){
-    store.dispatch({type:'add', payload: 1})
-  }
-}
-function add4(){
-  setTimeout(()=>{
-    store.dispatch({type:'add', payload: 1})
-  },2000)
-}
-
-function render(){
-  ReactDOM.render(<App value={store.getState()} 
-    onAdd1={()=>{store.dispatch({type:'add', payload: 1})}}
-    onAdd2={()=>{store.dispatch({type:'add', payload: 2})}}
-    onAdd3={add3}
-    onAdd4={add4}
-    />, document.getElementById('root'));
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
