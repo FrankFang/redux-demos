58dc351cdc8768b85c8c5586a9f40da9c1e77b5a
Frank Fang <FrankFang1990@gmail.com>
2018-11-05 21:38:42 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  add1(){
+    this.props.onAdd1()
+  }
+  add2(){
+    this.props.onAdd2()
+  }
+  add3(){
+    this.props.onAdd3()
+  }
+  add4(){
+    this.props.onAdd4()
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        你点击了 <span id="value">{this.props.value}</span> 次
+        <div>
+          <button id="add1" onClick={()=> this.add1()}>+1</button>
+          <button id="add2" onClick={()=> this.add2()}>+2</button>
+          <button id="add1IfOdd" onClick={()=>this.add3()}>如果是单数就+1</button>
+          <button id="add1After2Sec" onClick={()=>this.add4()}>两秒钟后+1</button>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stateChanger = (state, action)=>{
+  if(state === undefined){
+    return 0
+  }else{
+    if(action.type === 'add'){
+      var newState = state + action.payload
+      return newState
+    }else{
+      return state
+    }
+  }
+
+  return newState
+}
+const store = createStore(stateChanger)
+render()
+store.subscribe(()=>{
+  render()
+})
+
+function add3(){
+  if(store.getState() % 2 === 1){
+    store.dispatch({type:'add', payload: 1})
+  }
+}
+function add4(){
+  setTimeout(()=>{
+    store.dispatch({type:'add', payload: 1})
+  },2000)
+}
+
+function render(){
+  ReactDOM.render(<App value={store.getState()} 
+    onAdd1={()=>{store.dispatch({type:'add', payload: 1})}}
+    onAdd2={()=>{store.dispatch({type:'add', payload: 2})}}
+    onAdd3={add3}
+    onAdd4={add4}
+    />, document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
